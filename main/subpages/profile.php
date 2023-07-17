7efd55da6f999a5142c00cc981e91268c4173f67
elmaccho <maciek.chojnacki22@wp.pl>
2023-07-17 22:23:55 +0200

    profile

--- main/subpages/profile.php
+++ main/subpages/profile.php
@@ -1,19 +1,23 @@
 <div class="main__profile">
 
     <div class="main__bckImg">
+            <?php echo "<img class='bckImg' src='img/$bckImg' alt=''>"; ?>
+            <button class="bckImg__changeBtn changeBtn"><i class="fa-solid fa-camera"></i></button>
 
         <div class="main__profImg">
-            <img src="" alt="">
+            <?php echo "<img class='profImg' src='img/$profImg' alt=''>"; ?>
+
+            <button class="profImg__changeBtn changeBtn"><i class="fa-solid fa-camera"></i></button>
         </div>
     </div>
 
-    <?php 
-        echo $name;
-        echo $lastname;
-
-        echo "<img src='./img/$profImg' alt='' style='width: 120px;'>";
-
-        echo $biography;
-    ?>
+    <div class="main__row1">
+        <?php
+            echo "<span class='main__fullname'>$name $lastname</span>"; 
+            echo "<a class='redSett__btn' href='?page=settings'>
+                <i class='fa-solid fa-pencil'></i> Edytuj profil
+                </a>";
+        ?>
+    </div>
 
 </div>
