957ceed619497a4be1ce7944c4a31ed7932992a5
elmaccho <maciek.chojnacki22@wp.pl>
2023-07-25 22:22:44 +0200

    responsive sett

--- main/subpages/settings.php
+++ main/subpages/settings.php
@@ -2,177 +2,181 @@
 
 <div class="main__settings">
     <div class="sett__box">
-        <span class="span">
+        <div class="sett__col1">
             <h2 class="sett__title">
                 Zdjęcie profilowe
             </h2>
-            <form method="post" enctype="multipart/form-data">
+            <form method="post" class="sett__form" enctype="multipart/form-data">
                 <input type="hidden" name="MAX_FILE_SIZE" value="900000">
                 <input type="file" name="profileImg" id=""><br>
                 <input type="submit" value="Wyślij" class="sett__button">
             </form>
-            <?php
-                if (isset($_POST['profileImg'])) {
-                    $profileImg = $_POST['profileImg'];
-
-                    $conn = new mysqli("localhost", "root", "", "forumapporsmth");
-
-                    if ($conn->connect_error) {
-                        die("Błąd połączenia z bazą danych: " . $conn->connect_error);
-                    }
-                    $profileImg = $conn->real_escape_string($profileImg);
-                    $query = "UPDATE users SET profile_img = '$profileImg' WHERE email = '$email'";
-
-                    if ($conn->query($query) === TRUE) {
-
-                    } else {
-                        echo "Błąd podczas aktualizacji profilowego: " . $conn->error;
-                    }
-                    
-                    $conn->close();
-                }
-
-                if(isset($_FILES['profileImg'])){
-                    switch ($_FILES['profileImg']['error']) {
-                        case 0:
-                            if ($_FILES['profileImg']['type'] == "image/jpeg" || $_FILES['profileImg']['type'] == "image/png" || $_FILES['profileImg']['type'] == "image/webp") {
-                                move_uploaded_file($_FILES['profileImg']['tmp_name'], "./img/" . $_FILES['profileImg']['name']);
-
-                                $conn = new mysqli("localhost", "root", "", "forumapporsmth");
-
-                                if ($conn->connect_error) {
-                                    die("Błąd połączenia z bazą danych: " . $conn->connect_error);
-                                }
-
-                                $profileImg = $conn->real_escape_string($_FILES['profileImg']['name']);
-                                $query = "UPDATE users SET profile_img = '$profileImg' WHERE email = '$email'";
-
-                                if ($conn->query($query) === TRUE) {
-                                    header("Refresh:0");
-                                } else {
-                                    echo "Błąd podczas aktualizacji profilowego: " . $conn->error;
-                                }
-                                
-                                $conn->close();
-                            } else {
-                                echo "Niedozwolone rozszerzenie pliku";
-                            }
-                            break;
-                        case 1:
-                            echo "Za duży plik (PHP.ini)";
-                            break;
-                        case 2:
-                            echo "Za duży plik";
-                            break;
-                        case 3:
-                            echo "Niepełny plik";
-                            break;
-                        case 4:
-                            echo "Nie wybrano pliku";
-                            break;
-                        default:
-                            echo "Nieznany błąd. Prosimy o kontakt";
-                    }
-                }
-            ?>
-        </span>
+        </div>
+        <div class="sett__col2">
             <?php
                 echo "<img class='profile__img' src='./img/$profImg' alt=''>";
             ?>
+        </div>
+            <?php
+                // if (isset($_POST['profileImg'])) {
+                //     $profileImg = $_POST['profileImg'];
+
+                //     $conn = new mysqli("localhost", "root", "", "forumapporsmth");
+
+                //     if ($conn->connect_error) {
+                //         die("Błąd połączenia z bazą danych: " . $conn->connect_error);
+                //     }
+                //     $profileImg = $conn->real_escape_string($profileImg);
+                //     $query = "UPDATE users SET profile_img = '$profileImg' WHERE email = '$email'";
+
+                //     if ($conn->query($query) === TRUE) {
+
+                //     } else {
+                //         echo "Błąd podczas aktualizacji profilowego: " . $conn->error;
+                //     }
+                    
+                //     $conn->close();
+                // }
+
+                // if(isset($_FILES['profileImg'])){
+                //     switch ($_FILES['profileImg']['error']) {
+                //         case 0:
+                //             if ($_FILES['profileImg']['type'] == "image/jpeg" || $_FILES['profileImg']['type'] == "image/png" || $_FILES['profileImg']['type'] == "image/webp") {
+                //                 move_uploaded_file($_FILES['profileImg']['tmp_name'], "./img/" . $_FILES['profileImg']['name']);
+
+                //                 $conn = new mysqli("localhost", "root", "", "forumapporsmth");
+
+                //                 if ($conn->connect_error) {
+                //                     die("Błąd połączenia z bazą danych: " . $conn->connect_error);
+                //                 }
+
+                //                 $profileImg = $conn->real_escape_string($_FILES['profileImg']['name']);
+                //                 $query = "UPDATE users SET profile_img = '$profileImg' WHERE email = '$email'";
+
+                //                 if ($conn->query($query) === TRUE) {
+                //                     header("Refresh:0");
+                //                 } else {
+                //                     echo "Błąd podczas aktualizacji profilowego: " . $conn->error;
+                //                 }
+                                
+                //                 $conn->close();
+                //             } else {
+                //                 echo "Niedozwolone rozszerzenie pliku";
+                //             }
+                //             break;
+                //         case 1:
+                //             echo "Za duży plik (PHP.ini)";
+                //             break;
+                //         case 2:
+                //             echo "Za duży plik";
+                //             break;
+                //         case 3:
+                //             echo "Niepełny plik";
+                //             break;
+                //         case 4:
+                //             echo "Nie wybrano pliku";
+                //             break;
+                //         default:
+                //             echo "Nieznany błąd. Prosimy o kontakt";
+                //     }
+                // }
+            ?>
     </div>
     <div class="sett__box">
-        <span class="span">
+        <div class="sett__col1">
             <h2 class="sett__title">
                 Zdjęcie w tle
             </h2>
-            <form method="post" enctype="multipart/form-data">
+            <form method="post" class="sett__form" enctype="multipart/form-data">
                 <input type="hidden" name="MAX_FILE_SIZE" value="9000000">
                 <input type="file" name="backgroundImg" id=""><br>
                 <input type="submit" value="Wyślij" class="sett__button">
             </form>
-            <?php
-                if (isset($_POST['backgroundImg'])) {
-                    $backgroundImg = $_POST['backgroundImg'];
-
-                    $conn = new mysqli("localhost", "root", "", "forumapporsmth");
-
-                    if ($conn->connect_error) {
-                        die("Błąd połączenia z bazą danych: " . $conn->connect_error);
-                    }
-                    $backgroundImg = $conn->real_escape_string($backgroundImg);
-                    $query = "UPDATE users SET background_img = '$backgroundImg' WHERE email = '$email'";
-
-                    if ($conn->query($query) === TRUE) {
-
-                    } else {
-                        echo "Błąd podczas aktualizacji profilowego: " . $conn->error;
-                    }
-                    
-                    $conn->close();
-                }
-
-                if(isset($_FILES['backgroundImg'])){
-                    switch ($_FILES['backgroundImg']['error']) {
-                        case 0:
-                            if ($_FILES['backgroundImg']['type'] == "image/jpeg" || $_FILES['backgroundImg']['type'] == "image/png" || $_FILES['backgroundImg']['type'] == "image/webp") {
-                                move_uploaded_file($_FILES['backgroundImg']['tmp_name'], "./img/" . $_FILES['backgroundImg']['name']);
-
-                                $conn = new mysqli("localhost", "root", "", "forumapporsmth");
-
-                                if ($conn->connect_error) {
-                                    die("Błąd połączenia z bazą danych: " . $conn->connect_error);
-                                }
-
-                                $backgroundImg = $conn->real_escape_string($_FILES['backgroundImg']['name']);
-                                $query = "UPDATE users SET background_img = '$backgroundImg' WHERE email = '$email'";
-
-                                if ($conn->query($query) === TRUE) {
-                                    header("Refresh:0");
-                                } else {
-                                    echo "Błąd podczas aktualizacji profilowego: " . $conn->error;
-                                }
-                                
-                                $conn->close();
-                            } else {
-                                echo "Niedozwolone rozszerzenie pliku";
-                            }
-                            break;
-                        case 1:
-                            echo "Za duży plik (PHP.ini)";
-                            break;
-                        case 2:
-                            echo "Za duży plik";
-                            break;
-                        case 3:
-                            echo "Niepełny plik";
-                            break;
-                        case 4:
-                            echo "Nie wybrano pliku";
-                            break;
-                        default:
-                            echo "Nieznany błąd. Prosimy o kontakt";
-                    }
-                }
-            ?>
-        </span>
+        </div>
+        <div class="sett__col2">
             <?php
                 echo "<img class='background__img' src='./img/$bckImg' alt=''>";
             ?>
+        </div>
+            <?php
+                // if (isset($_POST['backgroundImg'])) {
+                //     $backgroundImg = $_POST['backgroundImg'];
+
+                //     $conn = new mysqli("localhost", "root", "", "forumapporsmth");
+
+                //     if ($conn->connect_error) {
+                //         die("Błąd połączenia z bazą danych: " . $conn->connect_error);
+                //     }
+                //     $backgroundImg = $conn->real_escape_string($backgroundImg);
+                //     $query = "UPDATE users SET background_img = '$backgroundImg' WHERE email = '$email'";
+
+                //     if ($conn->query($query) === TRUE) {
+
+                //     } else {
+                //         echo "Błąd podczas aktualizacji profilowego: " . $conn->error;
+                //     }
+                    
+                //     $conn->close();
+                // }
+
+                // if(isset($_FILES['backgroundImg'])){
+                //     switch ($_FILES['backgroundImg']['error']) {
+                //         case 0:
+                //             if ($_FILES['backgroundImg']['type'] == "image/jpeg" || $_FILES['backgroundImg']['type'] == "image/png" || $_FILES['backgroundImg']['type'] == "image/webp") {
+                //                 move_uploaded_file($_FILES['backgroundImg']['tmp_name'], "./img/" . $_FILES['backgroundImg']['name']);
+
+                //                 $conn = new mysqli("localhost", "root", "", "forumapporsmth");
+
+                //                 if ($conn->connect_error) {
+                //                     die("Błąd połączenia z bazą danych: " . $conn->connect_error);
+                //                 }
+
+                //                 $backgroundImg = $conn->real_escape_string($_FILES['backgroundImg']['name']);
+                //                 $query = "UPDATE users SET background_img = '$backgroundImg' WHERE email = '$email'";
+
+                //                 if ($conn->query($query) === TRUE) {
+                //                     header("Refresh:0");
+                //                 } else {
+                //                     echo "Błąd podczas aktualizacji profilowego: " . $conn->error;
+                //                 }
+                                
+                //                 $conn->close();
+                //             } else {
+                //                 echo "Niedozwolone rozszerzenie pliku";
+                //             }
+                //             break;
+                //         case 1:
+                //             echo "Za duży plik (PHP.ini)";
+                //             break;
+                //         case 2:
+                //             echo "Za duży plik";
+                //             break;
+                //         case 3:
+                //             echo "Niepełny plik";
+                //             break;
+                //         case 4:
+                //             echo "Nie wybrano pliku";
+                //             break;
+                //         default:
+                //             echo "Nieznany błąd. Prosimy o kontakt";
+                //     }
+                // }
+            ?>
     </div>
     <div class="sett__box">
-
+        <div class="sett__col1">
             <h2 class="sett__title">
                 Biogram
             </h2>
-
-            <form method="post" class="sett__bioForm">
+        </div>
+        <div class="sett__col2">
+            <form method="post" class="sett__form">
                 <span class="biography__span">
                     <?php 
                         echo "<textarea class='biography__input' name='biography' id='' maxlength='150'>$biography</textarea>";
                     ?>
 
                     <span class="textarea__counter">0/150</span>
-
                 </span>
                 <input type="submit" value="Zatwierdź" class="sett__button">
             </form>
@@ -195,15 +199,19 @@
                     $conn->close();
                 }
             ?>
+        </div>
     </div>
     <div class="sett__box">
+        <div class="sett__col1">
             <h2 class="sett__title">
                 Imię i nazwisko
             </h2>
-            <form method="post">
+        </div>
+        <div class="sett__col2">
+            <form method="post" class="sett__form">
                 <?php 
-                        echo "<input type='text' name='firstName' id='' value='$name'>";
-                        echo "<input type='text' name='lastName' id='' value='$lastname'>";
+                        echo "<input type='text' name='firstName' class='sett__input' value='$name'>";
+                        echo "<input type='text' name='lastName' class='sett__input' value='$lastname'>";
 
                         if(isset($_POST['firstName']) && isset($_POST['lastName']) && !empty($_POST['firstName']) && !empty($_POST['lastName'])){
                             $firstname = $_POST['firstName'];
@@ -227,15 +235,19 @@
                 ?>
                     <input type="submit" value="Zatwierdź" class="sett__button">
             </form>
+        </div>
     </div>
     <div class="sett__box">
+        <div class="sett__col1">
             <h2 class="sett__title">
                 Zmień hasło
             </h2>
-            <form method="post">
+        </div>
+        <div class="sett__col2">
+            <form method="post" class="sett__form">
                 <?php
-                    echo "<input type='password' name='actPassword' id='' placeholder='Stare hasło'>";
-                    echo "<input type='password' name='newPassword' id='' placeholder='Nowe hasło'>";
+                    echo "<input type='password' name='actPassword' class='sett__input' placeholder='Stare hasło'>";
+                    echo "<input type='password' name='newPassword' class='sett__input' placeholder='Nowe hasło'>";
 
                     if(isset($_POST['actPassword']) && isset($_POST['newPassword']) && !empty($_POST['actPassword']) && !empty($_POST['newPassword'])){
                         $actPassword = $_POST['actPassword'];
@@ -268,5 +280,6 @@
                 ?>
                     <input type="submit" value="Zatwierdź" class="sett__button">
             </form>
+        </div>
     </div>
 </div>
