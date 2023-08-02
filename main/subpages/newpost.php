ceb7bd48fe85e0cbb7a1b9c4262d727bb02fad9d
elmaccho <maciek.chojnacki22@wp.pl>
2023-08-02 17:20:10 +0200

    newpost

---
+++ main/subpages/newpost.php
@@ -0,0 +1,14 @@
+<div class="newpost">
+    <span class='newpost__title'>Dodaj post</span>
+
+    <form class='newpost__form'>
+        <input maxlength="64" class='newpost__input title__input' type="text" name="" placeholder="Tytuł">
+        <textarea maxlength='250' class='newpost__input desc__input' name="" placeholder="Opis"></textarea>
+        <input type="file" name="" multiple>
+        <select name="category">
+            <option value="comic">Komiks</option>
+            <option value="meme">Memy</option>
+        </select>
+        <input class='newpost__submit' type="submit" value="Dodaj post">
+    </form>
+</div>
