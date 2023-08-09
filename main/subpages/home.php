cc505cdf8787c41fc4171fbb7cda9c75cea64ee7
elmaccho <maciek.chojnacki22@wp.pl>
2023-08-09 21:51:58 +0200

    wyswietlanie komentarzy
    nie dziala XDD

--- main/subpages/home.php
+++ main/subpages/home.php
@@ -1,10 +1,7 @@
 <div class="home">
-    <?php
-        // include("./functions/showcomments.php");
-
-
+        <?php
         $conn = new mysqli("localhost","root","","forumapporsmth");
-        $query = "SELECT posts.id, posts.tytul, posts.opis, posts.tresc, posts.kategoria, posts.polubienia, posts.dislike, DATE_FORMAT(posts.data, '%d.%m.%Y') as data_formatowana, users.imie, users.nazwisko, users.profile_img, users.ranga FROM posts inner join users where posts.autor_id = users.id";
+        $query = "SELECT posts.id, posts.tytul, posts.opis, posts.tresc, posts.kategoria, posts.polubienia, posts.dislike, DATE_FORMAT(posts.data, '%d.%m.%Y') as data_formatowana, users.imie, users.nazwisko, users.profile_img, users.ranga FROM posts INNER JOIN users ON posts.autor_id = users.id";
         $result = $conn->query($query);
 
         if ($conn->connect_error) {
@@ -13,6 +10,7 @@
 
         if ($result->num_rows > 0) {
             while ($row = $result->fetch_assoc()) {
+                $postId = $row['id'];
                 $postTitle = $row['tytul'];
                 $postDesc = $row['opis'];
                 $postImg = $row['tresc'];
@@ -27,89 +25,104 @@
                 $postId = $row['id'];
 
                 echo "
-                <div class='post'>
-                $postId
-                    <button class='post__actionBtn'>
-                        <i class='fa-solid fa-ellipsis'></i>
-                    </button>
-            
-                    <span class='post__author__img'>
-                        <img src='./img/$postProfImg' alt='$postName $postLastname'>
-                        <div class='post__author__info'>
-                            <div class='author__name'>$postName $postLastname</div>
-                            <span class='author__rank'>$postRank</span>
-                        </div>
-                    </span>
-
-                    <span class='post__title-wrapper'>
-                        <p class='post__title'>$postTitle</p>
-                    </span>
-
-                    <span class='description__wrapper'>
-                        <div class='post__desc'>
-                            $postDesc
-                        </div>
-                        <button class='post__expand-button'>Zobacz więcej</button>
-                    </span>
-                    
-                    <div class='post__img'>
-                        <img src='postImg/$postImg' alt=''>
-                    </div>
-            
-                    <div class='post__info'>
-                        <span class='post__likes'>
-                            <button class='post__likeBtn'><i class='fa-regular fa-thumbs-up'></i></button>
-                            $postLikes
+                    <div class='post'>
+                    $postId
+                        <button class='post__actionBtn'>
+                            <i class='fa-solid fa-ellipsis'></i>
+                        </button>
+                
+                        <span class='post__author__img'>
+                            <img src='./img/$postProfImg' alt='$postName $postLastname'>
+                            <div class='post__author__info'>
+                                <div class='author__name'>$postName $postLastname</div>
+                                <span class='author__rank'>$postRank</span>
+                            </div>
                         </span>
-            
-                        <span class='post__dislikes'>
-                            <button class='post__dislikeBtn'><i class='fa-regular fa-thumbs-down'></i></button>
-                            $postDislikes
+
+                        <span class='post__title-wrapper'>
+                            <p class='post__title'>$postTitle</p>
                         </span>
-            
-                        <div class='post__date'>
-                            $postDate
+
+                        <span class='description__wrapper'>
+                            <div class='post__desc'>
+                                $postDesc
+                            </div>
+                            <button class='post__expand-button'>Zobacz więcej</button>
+                        </span>
+                        
+                        <div class='post__img'>
+                            <img src='postImg/$postImg' alt=''>
                         </div>
-                    </div>
-                    
+                
+                        <div class='post__info'>
+                            <span class='post__likes'>
+                                <button class='post__likeBtn'><i class='fa-regular fa-thumbs-up'></i></button>
+                                $postLikes
+                            </span>
+                
+                            <span class='post__dislikes'>
+                                <button class='post__dislikeBtn'><i class='fa-regular fa-thumbs-down'></i></button>
+                                $postDislikes
+                            </span>
+                
+                            <div class='post__date'>
+                                $postDate
+                            </div>
+                        </div>
+                        
+                
+                        <form class='post__comments' method='POST'>
+                                <input class='comments__input' type='text' name='comment' placeholder='Dodaj komentarz...'>
+                                <button class='comments__submit'><i class='fa-regular fa-comment-dots'></i></button>
+                        </form>
+                            <div class='post__comments-wrapper'>
+                            ";
+
+                        // $conn = new mysqli("localhost","root","","forumapporsmth");
+                        $commentQuery = "SELECT users.imie, users.nazwisko, users.profile_img, users.ranga, comments.autor_id, comments.tresc, comments.data_dodania FROM users INNER JOIN comments ON users.id = comments.autor_id WHERE comments.post_id = '$postId'";
+                        $commentResult = $conn->query($commentQuery);
+
+                        if($commentResult-> num_rows > 0){
+                            while($commentRow = $commentResult->fetch_assoc()){
+                                $userImie = $commentRow['imie'];
+                                $userNazwisko = $commentRow['nazwisko'];
+                                $userProfileImg = $commentRow['profile_img'];
+                                $userRank = $commentRow['ranga'];
+                                $commentAutorId = $commentRow['autor_id'];
+                                $commentTresc = $commentRow['tresc'];
+                                $commentDataDodania = $commentRow['data_dodania'];
+
+                                echo "
+                                    <div class='post__comm'>
+                                    <button class='comm__actionBtn'><i class='fa-solid fa-ellipsis'></i></button>
             
-                    <form class='post__comments' method='POST'>
-                            <input class='comments__input' type='text' name='comment' placeholder='Dodaj komentarz...'>
-                            <button class='comments__submit'><i class='fa-regular fa-comment-dots'></i></button>
-                    </form>
-                    <div class='post__comments-wrapper'>
-                    e
-                    ";
-
-                        $postId = $row['id'];
-                        $commentsQuery = "SELECT * FROM comments WHERE post_id = '$postId'";
-                        $commentsResult = $conn->query($commentsQuery);
-
-                        if($commentsResult->num_rows > 0){
-                            while($commentRow = $commentsResult->fetch_assoc()){
-                                $commentContent = $commentRow['tresc'];
-                                $commentAuthorId = $commentRow['author_id'];
-
-                                $commentUserQuery = "SELECT imie, nazwisko, profile_img FROM users WHERE id = '$commentAuthorId'";
-                                $commentUserResult = $conn->query($commentUserQuery);
-
-                                if ($commentUserResult->num_rows > 0) {
-                                    $commentAuthorData = $commentUserResult->fetch_assoc();
-                                    $commentAuthorName = $commentAuthorData['imie'] . ' ' . $commentAuthorData['nazwisko'];
-                                    $commentAuthorProfileImg = $commentAuthorData['profile_img'];
-
-                                    // Wyświetl komentarz wewnątrz pętli komentarzy
-                                    echo "
-                                    <div class='comment'>
-                                        <span class='comment__author__img'>
-                                            <img src='./img/$commentAuthorProfileImg' alt='$commentAuthorName'>
+                                    <div class='comm__row-info'>
+                                        <div class='comm__info-img'>
+                                            <img src='./img/$userProfileImg' alt='$userImie $userNazwisko'>
+                                        </div>
+                                        <div class='comm__info-userInfo'>
+                                            <span class='comm__userName'>
+                                                $userImie $userNazwisko
+                                            </span>
+                                            <div class='comm__userRank'>
+                                                $userRank
+                                            </div>
+                                        </div>
+                                        <span class='comm__dateAdded'>
+                                            $commentDataDodania
                                         </span>
-                                        <span class='comment__content'>$commentContent</span>
-                                    </div>";
-                                }
+                                    </div>
+                                    <div class='comm__row-content'>
+                                        $commentTresc
+                                        <hr>
+                                    </div>
+            
+                                </div>
+                                ";
                             }
                         }
-                    
+                        $conn->close();
+
                     echo "
                         </div>
                     </div>";
--- main/subpages/settings.php
+++ main/subpages/settings.php
@@ -99,68 +99,68 @@
             ?>
         </div>
             <?php
-                // if (isset($_POST['backgroundImg'])) {
-                //     $backgroundImg = $_POST['backgroundImg'];
+                if (isset($_POST['backgroundImg'])) {
+                    $backgroundImg = $_POST['backgroundImg'];
 
-                //     $conn = new mysqli("localhost", "root", "", "forumapporsmth");
+                    $conn = new mysqli("localhost", "root", "", "forumapporsmth");
 
-                //     if ($conn->connect_error) {
-                //         die("Błąd połączenia z bazą danych: " . $conn->connect_error);
-                //     }
-                //     $backgroundImg = $conn->real_escape_string($backgroundImg);
-                //     $query = "UPDATE users SET background_img = '$backgroundImg' WHERE email = '$email'";
+                    if ($conn->connect_error) {
+                        die("Błąd połączenia z bazą danych: " . $conn->connect_error);
+                    }
+                    $backgroundImg = $conn->real_escape_string($backgroundImg);
+                    $query = "UPDATE users SET background_img = '$backgroundImg' WHERE email = '$email'";
 
-                //     if ($conn->query($query) === TRUE) {
+                    if ($conn->query($query) === TRUE) {
 
-                //     } else {
-                //         echo "Błąd podczas aktualizacji profilowego: " . $conn->error;
-                //     }
+                    } else {
+                        echo "Błąd podczas aktualizacji profilowego: " . $conn->error;
+                    }
                     
-                //     $conn->close();
-                // }
+                    $conn->close();
+                }
 
-                // if(isset($_FILES['backgroundImg'])){
-                //     switch ($_FILES['backgroundImg']['error']) {
-                //         case 0:
-                //             if ($_FILES['backgroundImg']['type'] == "image/jpeg" || $_FILES['backgroundImg']['type'] == "image/png" || $_FILES['backgroundImg']['type'] == "image/webp") {
-                //                 move_uploaded_file($_FILES['backgroundImg']['tmp_name'], "./img/" . $_FILES['backgroundImg']['name']);
+                if(isset($_FILES['backgroundImg'])){
+                    switch ($_FILES['backgroundImg']['error']) {
+                        case 0:
+                            if ($_FILES['backgroundImg']['type'] == "image/jpeg" || $_FILES['backgroundImg']['type'] == "image/png" || $_FILES['backgroundImg']['type'] == "image/webp") {
+                                move_uploaded_file($_FILES['backgroundImg']['tmp_name'], "./img/" . $_FILES['backgroundImg']['name']);
 
-                //                 $conn = new mysqli("localhost", "root", "", "forumapporsmth");
+                                $conn = new mysqli("localhost", "root", "", "forumapporsmth");
 
-                //                 if ($conn->connect_error) {
-                //                     die("Błąd połączenia z bazą danych: " . $conn->connect_error);
-                //                 }
+                                if ($conn->connect_error) {
+                                    die("Błąd połączenia z bazą danych: " . $conn->connect_error);
+                                }
 
-                //                 $backgroundImg = $conn->real_escape_string($_FILES['backgroundImg']['name']);
-                //                 $query = "UPDATE users SET background_img = '$backgroundImg' WHERE email = '$email'";
+                                $backgroundImg = $conn->real_escape_string($_FILES['backgroundImg']['name']);
+                                $query = "UPDATE users SET background_img = '$backgroundImg' WHERE email = '$email'";
 
-                //                 if ($conn->query($query) === TRUE) {
-                //                     header("Refresh:0");
-                //                 } else {
-                //                     echo "Błąd podczas aktualizacji profilowego: " . $conn->error;
-                //                 }
+                                if ($conn->query($query) === TRUE) {
+                                    header("Refresh:0");
+                                } else {
+                                    echo "Błąd podczas aktualizacji profilowego: " . $conn->error;
+                                }
                                 
-                //                 $conn->close();
-                //             } else {
-                //                 echo "Niedozwolone rozszerzenie pliku";
-                //             }
-                //             break;
-                //         case 1:
-                //             echo "Za duży plik (PHP.ini)";
-                //             break;
-                //         case 2:
-                //             echo "Za duży plik";
-                //             break;
-                //         case 3:
-                //             echo "Niepełny plik";
-                //             break;
-                //         case 4:
-                //             echo "Nie wybrano pliku";
-                //             break;
-                //         default:
-                //             echo "Nieznany błąd. Prosimy o kontakt";
-                //     }
-                // }
+                                $conn->close();
+                            } else {
+                                echo "Niedozwolone rozszerzenie pliku";
+                            }
+                            break;
+                        case 1:
+                            echo "Za duży plik (PHP.ini)";
+                            break;
+                        case 2:
+                            echo "Za duży plik";
+                            break;
+                        case 3:
+                            echo "Niepełny plik";
+                            break;
+                        case 4:
+                            echo "Nie wybrano pliku";
+                            break;
+                        default:
+                            echo "Nieznany błąd. Prosimy o kontakt";
+                    }
+                }
             ?>
     </div>
     <div class="sett__box">
